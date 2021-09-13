063ccf67b1da023fdfa37f15838fb0c9995b94be
Valentine <valentine.nazarov@gmail.com>
2021-09-13 16:07:42 +0300

    feat: add st-dns-stats

--- src/st-dns-stats.js
+++ src/st-dns-stats.js
@@ -1,5 +1,3 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
  *
@@ -22,7 +20,16 @@ import { NotImplementedError } from '../extensions/index.js';
  * }
  *
  */
-export default function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getDNSStats(domains) {
+    const DNS = {};
+    for (let domain of domains) {
+        domain = domain.split('.').reverse();
+        let record = '';
+        for (let subDomain of domain) {
+            record += "." + subDomain;
+            if (!(record in DNS)) DNS[record] = 1;
+            else DNS[record]++;
+        }
+    }
+    return DNS;
 }
