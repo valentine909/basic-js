075a5de175159b1541a6f57544c34340e2579c03
Valentine <valentine.nazarov@gmail.com>
2021-09-13 15:18:20 +0300

    feat: add simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,28 +1,37 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js';
 
+let chainMaker;
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
-export default {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+export default chainMaker = {
+    chain: [],
+    getLength() {
+        return this.chain.length;
+    },
+    addLink(value) {
+        this.chain.push(String(value));
+        return this;
+    },
+    removeLink(position) {
+        if (position < 1 || position > this.chain.length - 1 || !Number(position)) {
+            this.chain = [];
+            throw new Error('You can\'t remove incorrect link!');
+        }
+        this.chain.splice(position - 1, 1);
+        return this;
+    },
+    reverseChain() {
+      this.chain.reverse();
+      return this;
+    },
+    finishChain() {
+      let result = '( ' + this.chain.join(' )~~( ') + ' )';
+      this.chain = [];
+      return result;
+    }
 };
+
+// console.log(chainMaker.addLink(function () { }).addLink('2nd').addLink('3rd').removeLink(2).reverseChain().finishChain());
+// console.log(chainMaker.addLink(1).addLink(2).addLink(3).removeLink());
