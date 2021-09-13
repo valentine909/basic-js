5b1b37a338dac48d303b8ae0a5f6fcc458255b8d
Valentine <valentine.nazarov@gmail.com>
2021-09-13 11:23:15 +0300

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,3 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Create a repeating string based on the given parameters
  *  
@@ -15,7 +13,14 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options ) {
+    str = String(str);
+    let repeatTimes = (options['repeatTimes'] === undefined) ? 1 : options['repeatTimes'];
+    let separator = (options['separator'] === undefined) ? '+' : options['separator'];
+    let addition = (options['addition'] === undefined) ? "" : String(options['addition']);
+    let additionRepeatTimes = (options['additionRepeatTimes'] === undefined) ? 1 : options['additionRepeatTimes'];
+    let additionSeparator = (options['additionSeparator'] === undefined) ? "|" : options['additionSeparator'];
+
+    let addToken = new Array(additionRepeatTimes).fill(addition).join(additionSeparator);
+    return new Array(repeatTimes).fill(str + addToken).join(separator);
 }
