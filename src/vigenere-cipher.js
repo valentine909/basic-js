4104999979b3006c67da625a586d96e5150a9fa9
Valentine <valentine.nazarov@gmail.com>
2021-09-14 16:47:17 +0300

    feat: add vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,56 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct=true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    [message, key] = this.processInput(message, key);
+    let encryptedMessage = '';
+    let pattern = this.specialSymbolsRegexPattern();
+    let specialSymbolsCount = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (pattern.test(message[i])) {
+        encryptedMessage += message[i];
+        specialSymbolsCount++;
+      } else {
+        let newCharCode = (message.codePointAt(i) + key.codePointAt(i - specialSymbolsCount) - 65);
+        if (newCharCode > 90) newCharCode = newCharCode - 26;
+        encryptedMessage += String.fromCharCode(newCharCode);
+      }
+    }
+    return (this.direct) ? encryptedMessage : encryptedMessage.split("").reverse().join("");
+  }
+  decrypt(message, key) {
+    [message, key] = this.processInput(message, key);
+    let decryptedMessage = '';
+    let pattern = this.specialSymbolsRegexPattern();
+    let specialSymbolsCount = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (pattern.test(message[i])) {
+        decryptedMessage += message[i];
+        specialSymbolsCount++;
+      } else {
+        let newCharCode = (message.codePointAt(i) - key.codePointAt(i - specialSymbolsCount) + 65);
+        if (newCharCode < 65) newCharCode = newCharCode + 26;
+        decryptedMessage += String.fromCharCode(newCharCode);
+      }
+    }
+    return (this.direct) ? decryptedMessage : decryptedMessage.split("").reverse().join("");
+  }
+  constructKey(message, key) {
+    if (key.length >= message.length) return key.slice(0, key.length).toUpperCase();
+    else if (key.length < message.length) {
+      return key.repeat(Math.ceil(message.length / key.length)).slice(0, message.length).toUpperCase();
+    }
+  }
+  specialSymbolsRegexPattern(){
+    return new RegExp(/[^a-zA-Z]/);
+  }
+  processInput(message, key) {
+    if (message === undefined || key === undefined) throw Error('Incorrect arguments!');
+    message = message.toUpperCase();
+    key = this.constructKey(message, key);
+    return [message, key];
   }
 }
